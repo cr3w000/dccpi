d3ba5c6e7381a374be99aec2940c455faca65b2d
Tom Bartulec <cr3w000@gmail.com>
2021-04-20 22:06:05 +0100

    Added Train control and Settings screens

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, NoTransition, WipeTransition
 from kivy.properties import (
     NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty, StringProperty )
+from kivy.core.window import Window
 from end_to_end_distance import End_To_End
 import ultrasonic_distance
 import threading
@@ -44,7 +46,7 @@ class Semafor(Widget):
 class TextRow(Widget):
     pass
 
-class Main_Screen(Widget):
+class Main_Screen(Screen):
     buttonEtE = ObjectProperty(None)
     buttonStop = ObjectProperty(None)
     semafor = ObjectProperty(None)
@@ -59,6 +61,7 @@ class Main_Screen(Widget):
 
     def __init__(self, **kwargs):
         super(Main_Screen, self).__init__(**kwargs)
+        #super().__init__(**kwargs)
         self.dist = ''
         Clock.schedule_interval(self.update, .1)
 
@@ -86,11 +89,49 @@ class Main_Screen(Widget):
                 e.gentle_start()
             self.semafor.color = (0, 1, 0, 1)
 
+class Train_Screen(Screen):
+    buttonBack = ObjectProperty(None)
+    buttonStop = ObjectProperty(None)
+    semafor = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def train_stop(self):
+        if (self.buttonStop.state == "normal"):
+            print("STOP UP")
+        else:
+            print("STOP DOWN")
+            e.emergency_stop()
+            self.semafor.color = (1, 0, 0, 1)
+
+
+class Settings_Screen(Screen):
+    buttonBack = ObjectProperty(None)
+    buttonStop = ObjectProperty(None)
+    semafor = ObjectProperty(None)
+
+    def train_stop(self):
+        if (self.buttonStop.state == "normal"):
+            print("STOP UP")
+        else:
+            print("STOP DOWN")
+            e.emergency_stop()
+            self.semafor.color = (1, 0, 0, 1)
+
 
 class RpiVlakApp(App):
     def build(self):
-        return Main_Screen()
+        sm = ScreenManager(transition=NoTransition())
+        sm.add_widget(Main_Screen(name='main'))
+        sm.add_widget(Train_Screen(name='train'))
+        sm.add_widget(Settings_Screen(name='settings'))
+
+        return sm
+#        return Main_Screen()
 
 
 if __name__ == '__main__':
+    Window.size = (640, 480)
+    Window.fullscreen = True
     RpiVlakApp().run()
